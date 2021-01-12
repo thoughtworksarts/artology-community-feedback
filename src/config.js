b880902e342e9afdeb3211c9c65bd21cf1830b19
flavia.dessoldi <flavia.dessoldi@thoughtworks.com>
2021-01-12 14:58:47 -0300

    Add Seismic Percussion artwork

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export const artworks = {
   afl: "A Father's Lullaby",
   output: 'Output',
+  seismicp:"Seismic Percussion"
 };
 export const environments = ['indoor', 'outdoor', 'other'];
 export const categories = ['regions', 'gender', 'skintone', 'ethnicity'];
@@ -13,6 +14,8 @@ export const aflEffectiveScoreColumns = [20, 23, 25]; // Column numbers for effe
 export const aflInteractiveExperienceColumns = [23, 25]; // Column numbers for interactive experience score data for A Fathers Lulliby
 export const outputEffectiveScoreColumns = [27, 30, 32]; // Column numbers for effective score data for Output
 export const outputInteractiveExperienceColumns = [30, 32]; // Column numbers for interactive experience score data for Output
+export const seismicpEffectiveScoreColumns = [43, 47, 49]; // Column numbers for effective score data for Seismic Percussion
+export const seismicpInteractiveExperienceColumns = [47, 49]; // Column numbers for interactive experience score data for Seismic Percussion
 export const usageTypeColumn = 19; // Column number for usage type (indoor/outdoor) data
 export const deviceCategoryColumn = 11; // Column number for device category data (high-end, low-end, android)
 export const releaseVersionColumn = 2; // Column Number for release dates/versions
@@ -24,9 +27,11 @@ export const ethnicityColumn = 7; // Column number for ethnicity data
 export const esColumnGroup = {
   afl: aflEffectiveScoreColumns,
   output: outputEffectiveScoreColumns,
+  seismicp: seismicpEffectiveScoreColumns,
 };
 
 export const iesColumnGroup = {
   afl: aflInteractiveExperienceColumns,
   output: outputInteractiveExperienceColumns,
+  seismicp: seismicpInteractiveExperienceColumns
 };
